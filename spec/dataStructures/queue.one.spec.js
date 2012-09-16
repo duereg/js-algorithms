b83b0a14936517c912276793511f4be919488b7d
Matt Blair <duereg@hotmail.com>
2012-09-16 11:15:14 +0800

    Added Queue Data Structure, fixed typo in stack test, added algorithm to test if binary tree is balanced. No tests for isBalanced() algorithm yet.

---
+++ spec/dataStructures/queue.one.spec.js
@@ -0,0 +1,48 @@
+var Queue = require("../../lib/dataStructures/queue.js"); 
+
+describe('When adding an element onto a Queue', function () {
+	var myQueue; 
+	var firstValue = 1; 
+
+	beforeEach(function() {		
+		myQueue = new Queue();
+		myQueue.add(firstValue); 
+	});
+
+	afterEach(function() {
+		myQueue = null;
+	});
+
+	it('the Queues` length should be 1', function () { 
+		expect(myQueue.length).toBe(1);
+	});
+
+	it('peek should show the item', function() {
+		expect(myQueue.peek()).toBe(firstValue);
+	});
+
+});
+
+describe('When the Queue contains one element and your remove it', function () {
+	var myQueue;
+	var testValue = 1;
+
+	beforeEach(function() {		
+		myQueue = new Queue();
+		myQueue.add(testValue); 
+		myQueue.remove(testValue); 
+		
+	});
+
+	afterEach(function() {
+		myQueue = null;
+	});
+
+	it('the Queue`s length should be zero', function () { 
+		expect(myQueue.length).toBe(0);
+	});
+
+	it('peek should be null', function() {
+		expect(myQueue.peek()).toBe(null);
+	});
+});
