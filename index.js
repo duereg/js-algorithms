1acfe0831ff17e8ea65c785d4a0fd50737c50fb1
Matt <duereg@hotmail.com>
2014-12-01 16:04:43 -0800

    add shuffle to module output

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var dataStructures = require("./lib/dataStructures/");
 var sorting = require("./lib/algorithms/11-sorting/");
+var shuffle = require("./lib/algorithms/1-strings/shuffle");
 
 module.exports = {
   dataStructures: dataStructures,
   algorithms: {
-    sorting: sorting
+    sorting: sorting,
+    shuffle: shuffle
   }
 }
