6f34b10d22df04389ab486a00700480ac10a4e70
Matt Blair <matt@mattblair.co>
2016-11-08 15:32:27 -0800

    index eslint fix

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const dataStructures = require("./lib/dataStructures/");
-const sorting = require("./lib/algorithms/11-sorting/");
-const shuffle = require("./lib/algorithms/1-strings/shuffle");
-const enclosingCircle = require("./lib/algorithms/4-searching/smallestEnclosingCircle");
+const dataStructures = require('./lib/dataStructures/');
+const sorting = require('./lib/algorithms/11-sorting/');
+const shuffle = require('./lib/algorithms/1-strings/shuffle');
+const enclosingCircle = require('./lib/algorithms/4-searching/smallestEnclosingCircle');
 
 module.exports = {
   dataStructures,
   algorithms: {
     sorting,
     shuffle,
-    enclosingCircle
-  }
-}
+    enclosingCircle,
+  },
+};
