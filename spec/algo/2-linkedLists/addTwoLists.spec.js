74017cee91b02ec968cfd2980763e8985df84566
Matt Blair <matt@mattblair.co>
2017-11-09 14:16:00 -0800

    fix addTwoLists implementation

--- spec/algo/2-linkedLists/addTwoLists.spec.js
+++ spec/algo/2-linkedLists/addTwoLists.spec.js
@@ -5,29 +5,28 @@ describe('When using addTwoLists() on two linked lists', () => {
   let oneTwoThree,
     twoThreeFour,
     nineNineOne,
-    nineNineOneOne;
+    nineNineOneOne,
+    nineNineNine,
+    one;
 
   beforeEach(() => {
     oneTwoThree = new LinkedList();
-    oneTwoThree.add(1);
-    oneTwoThree.add(2);
-    oneTwoThree.add(3);
+    oneTwoThree.add(1).add(2).add(3);
 
     twoThreeFour = new LinkedList();
-    twoThreeFour.add(2);
-    twoThreeFour.add(3);
-    twoThreeFour.add(4);
+    twoThreeFour.add(2).add(3).add(4);
 
     nineNineOne = new LinkedList();
-    nineNineOne.add(9);
-    nineNineOne.add(9);
-    nineNineOne.add(1);
+    nineNineOne.add(9).add(9).add(1);
 
     nineNineOneOne = new LinkedList();
-    nineNineOneOne.add(9);
-    nineNineOneOne.add(9);
-    nineNineOneOne.add(1);
-    nineNineOneOne.add(1);
+    nineNineOneOne.add(9).add(9).add(1).add(1);
+
+    nineNineNine = new LinkedList();
+    nineNineNine.add(9).add(9).add(9);
+
+    one = new LinkedList();
+    one.add(1);
   });
 
   it('given 1->2->3 and 2->3->4, 3->5->7 returned.', () => {
@@ -62,6 +61,20 @@ describe('When using addTwoLists() on two linked lists', () => {
     expect(result.data).toBe(3);
     result = result.next;
     expect(result.data).toBe(6);
+    result = result.next;
+    expect(result.data).toBe(1);
+    result = result.next;
+    expect(result.data).toBe(undefined);
+  });
+
+  it('given 9->9->9 and 1, 0->0->0->1 returned.', () => {
+    let result = addTwoLists(nineNineNine.start, one.start);
+
+    expect(result.data).toBe(0);
+    result = result.next;
+    expect(result.data).toBe(0);
+    result = result.next;
+    expect(result.data).toBe(0);
     result = result.next;
     expect(result.data).toBe(1);
     result = result.next;
